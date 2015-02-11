c4e3aa48bf07ebe722d8e6ca8217e910e78fd347
Kate <kbalducci@hotmail.com>
2015-02-11 15:15:42 -0600

    sign in working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users do
+    resources :posts
+  end
 
-  root 'users#index'
+  get 'users/index' => 'users#index'
+  get 'posts/show' => 'posts#show'
+  get 'users/show' => 'users#show'
+  root 'users#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
