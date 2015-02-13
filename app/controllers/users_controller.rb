532ce6e474bb611dc0331ffd370a0f56ab1641a7
Kate <kbalducci@hotmail.com>
2015-02-12 22:38:52 -0600

    added users profile page info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!, except: :index
+
+
   def show
     User.find(params[:id])
+    @user = User.find(params[:id])
+    posts = Post.all
+    @posts = posts.where(user_id:@user_id)
   end
 end
