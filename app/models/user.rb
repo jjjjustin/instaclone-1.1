d6d14d24fc839d5f4ad92a5c84acdd58c94ac1fd
Kate <kbalducci@hotmail.com>
2015-02-12 18:49:25 -0600

    fixed avatar showing, model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
 
  has_attached_file :avatar,
  :styles => { :medium => "300x300>", :thumb => "100x100>" },
- :storage => :s3
- :bucket => "kbjsinstabucket"
+ :url =>':s3_domain_url',
+ :path => ':class/:attachment/:id_partition/:style/:filename',
  :default_url => "/images/:style/missing.png"
  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
