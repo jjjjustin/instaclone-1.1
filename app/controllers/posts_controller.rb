8bff811039c5f5d023a7378ec14c0c2b4856f286
Kate <kbalducci@hotmail.com>
2015-03-08 01:07:24 -0600

    updated post create controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,19 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
 
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to users_show_path(current_user), notice: 'Post was successfully created.' }
+      else
+        format.html { redirect_to root_path, notice: "Something went wrong" }
+      end
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:caption, :picture)
+    end
 end
